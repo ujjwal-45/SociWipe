fec494a3687975d1c586fe065020cdb83c5e0df6
Ujjwal <ujjwalchoudhary065@gmail.com>
2023-06-02 12:43:04 +0530

    updated name

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          SociWipe
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
