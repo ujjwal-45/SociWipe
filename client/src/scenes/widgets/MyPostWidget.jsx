7148e9267d361fb4cd874dbaad6064edea6db52e
Ujjwal Choudhary <106880198+ujjwal-45@users.noreply.github.com>
2023-06-03 20:45:25 +0530

    Update MyPostWidget.jsx

--- client/src/scenes/widgets/MyPostWidget.jsx
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -46,7 +46,7 @@ const MyPostWidget = ({ picturePath }) => {
       formData.append("picturePath", image.name);
     }
 
-    const response = await fetch(`hhttps://sociowi.onrender.com/posts`, {
+    const response = await fetch(`https://sociowi.onrender.com/posts`, {
       method: "POST",
       headers: { Authorization: `Bearer ${token}` },
       body: formData,
