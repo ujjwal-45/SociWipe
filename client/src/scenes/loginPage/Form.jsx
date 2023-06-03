ea0ab133e02c22c43da42f1a4c4a2bf89966b0a1
Ujjwal Choudhary <106880198+ujjwal-45@users.noreply.github.com>
2023-06-03 19:50:05 +0530

    Update Form.jsx

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -64,7 +64,7 @@ const Form = () => {
     formData.append("picturePath", values.picture.name);
 
     const savedUserResponse = await fetch(
-      "http://localhost:10000/auth/register",
+      "https://sociowi.onrender.com/auth/register",
       {
         method: "POST",
         body: formData,
@@ -79,7 +79,7 @@ const Form = () => {
   };
 
   const login = async (values, onSubmitProps) => {
-    const loggedInResponse = await fetch("http://localhost:10000/auth/login", {
+    const loggedInResponse = await fetch("https://sociowi.onrender.com/auth/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(values),
